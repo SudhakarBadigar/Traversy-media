4e272b7f7671d0caf81064246c0054c2d32eea7c
SudhakarBadigar <sudhakar.badigar87@gmail.com>
2023-03-09 16:59:33 +0530

    js

---
+++ javascript/fundamentals/backtic.js
@@ -0,0 +1,4 @@
+fname = "Ilya";
+console.log(`hello ${1}`); //hello1
+console.log(`hello ${"fname"}`); //hello fname
+console.log(`hello ${fname}`); //hello Ilya
---
+++ javascript/fundamentals/nan.js
@@ -0,0 +1,22 @@
+console.log(NaN +1);
+console.log(3 * NaN);
+console.log("not a number" / 0);
+console.log(NaN ** 0);
+console.log(NaN / 0);
+console.log("...............");
+let age = 100;
+age = null;
+console.log(age);
+console.log("...........");
+
+let fname;
+console.log(fname);
+let notaNumber = (NaN /2);
+console.log(notaNumber);
+console.log(typeof notaNumber);
+let infi = (1/0);
+console.log(infi);
+console.log(typeof infi);
+
+console.log(typeof null);
+console.log(typeof alert);
